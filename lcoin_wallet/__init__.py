9aa3bc4a59fde83ae841ce325e6e4438761baa55
jaredthejellyfish <ger.almenara@gmail.com>
2022-03-22 09:58:51 +0100

    ENVIRONMENT VARIABLES

--- lcoin_wallet/__init__.py
+++ lcoin_wallet/__init__.py
@@ -7,8 +7,8 @@ import os
 
 app = Flask(__name__)
 
-app.config['SECRET_KEY'] = '5e971d11505ed2faf96da8341de6f576aac555865d35d9f465f9073636ba46a8'
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URI')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 app.register_error_handler(404, page_not_found)
