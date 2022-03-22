484bc5e4bf6ec06df723454e8253e06094c17300
jaredthejellyfish <ger.almenara@gmail.com>
2022-03-22 09:23:14 +0100

    Profile fixed

---
+++ lcoin_wallet/__init__.py
@@ -0,0 +1,22 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+from lcoin_wallet.error_handlers import page_not_found
+
+app = Flask(__name__)
+
+app.config['SECRET_KEY'] = '5e971d11505ed2faf96da8341de6f576aac555865d35d9f465f9073636ba46a8'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+app.register_error_handler(404, page_not_found)
+
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
+
+
+from lcoin_wallet import routes
