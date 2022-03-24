0c5d8b4d01849fb38332e4f28f54bff464553d64
jaredthejellyfish <ger.almenara@gmail.com>
2022-03-25 00:41:05 +0100

    fixed odd spacing

--- lcoin_wallet/static/password-eye.js
+++ lcoin_wallet/static/password-eye.js
@@ -2,7 +2,6 @@ const togglePassword = document.querySelector("#togglePassword");
 const password = document.querySelector("#password");
 const confirm_password = document.querySelector("#confirm-password");
 
-
 togglePassword.addEventListener("click", function () {
   // toggle the type attribute
   const type =
@@ -14,7 +13,6 @@ togglePassword.addEventListener("click", function () {
   } catch (e) {
     //pass
   }
-
   // toggle the icon
   this.classList.toggle("fa-eye-slash");
 });
