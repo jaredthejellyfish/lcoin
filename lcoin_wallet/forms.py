66e92271ed2cba9b748fd03fe1c2182ac1a9bb54
jaredthejellyfish <ger.almenara@gmail.com>
2022-03-25 00:22:00 +0100

    added password vis icon

--- lcoin_wallet/forms.py
+++ lcoin_wallet/forms.py
@@ -11,9 +11,11 @@ class RegistrationForm(FlaskForm):
                            validators=[DataRequired(), Length(min=2, max=20)])
     email = StringField('Email',
                         validators=[DataRequired(), Email()])
-    password = PasswordField('Password', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[
+                             DataRequired()], id='password')
     confirm_password = PasswordField('Confirm Password',
-                                     validators=[DataRequired(), EqualTo('password')])
+                                     validators=[DataRequired(), EqualTo('password')], id='confirm-password')
+
     submit = SubmitField('Sign Up')
 
     def validate_username(self, username):
@@ -30,9 +32,11 @@ class RegistrationForm(FlaskForm):
 
 class LoginForm(FlaskForm):
     email_or_username = StringField('Email or Username',
-                        validators=[DataRequired()])
-    password = PasswordField('Password', validators=[DataRequired()])
+                                    validators=[DataRequired()])
+    password = PasswordField('Password', validators=[
+                             DataRequired()], id='password')
     remember = BooleanField('Remember Me')
+
     submit = SubmitField('Login')
 
 
@@ -42,7 +46,8 @@ class UpdateAccountForm(FlaskForm):
     email = StringField('Email',
                         validators=[DataRequired(), Email()])
 
-    picture = FileField('Update Profile Picture', validators=[FileAllowed(['jpg', 'jpeg', 'png'])])
+    picture = FileField('Update Profile Picture', validators=[
+                        FileAllowed(['jpg', 'jpeg', 'png'])])
 
     submit = SubmitField('Update')
 
@@ -59,39 +64,47 @@ class UpdateAccountForm(FlaskForm):
             if user:
                 raise ValidationError('This email is already in use...')
 
+
 class SendMoneyForm(FlaskForm):
     to = StringField('Username',
-                           validators=[DataRequired(), Length(min=2, max=20)])
-                           
+                     validators=[DataRequired(), Length(min=2, max=20)])
+
     concept = TextAreaField('Concept', validators=[Length(max=241)])
 
     amount = FloatField('Amount', validators=[DataRequired()])
 
     submit = SubmitField('Send')
+
 
 class RequestMoneyFrom(FlaskForm):
     to = StringField('Username',
-                           validators=[DataRequired(), Length(min=2, max=20)])
-                           
+                     validators=[DataRequired(), Length(min=2, max=20)])
+
     concept = TextAreaField('Concept', validators=[Length(max=241)])
 
     amount = FloatField('Amount', validators=[DataRequired()])
 
     submit = SubmitField('Send')
+
 
 class RequestResetForm(FlaskForm):
     email = StringField('Email',
                         validators=[DataRequired(), Email()])
     submit = SubmitField('Request Password Reset')
-    
+
     def validate_email(self, email):
         user = User.query.filter_by(email=email.data).first()
         if not user:
-            raise ValidationError('There is no account with this email. You must register first...')
+            raise ValidationError(
+                'There is no account with this email. You must register first...')
+
 
 class ResetPasswordForm(FlaskForm):
-    password = PasswordField('Password', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[
+                             DataRequired()], id='password')
+
     confirm_password = PasswordField('Confirm Password',
-                                     validators=[DataRequired(), EqualTo('password')])
-    
+                                     validators=[DataRequired(), EqualTo('password')], id='confirm-password')
+
+
     submit = SubmitField('Reset Password')
---
+++ lcoin_wallet/static/password-eye.js
@@ -0,0 +1,20 @@
+const togglePassword = document.querySelector("#togglePassword");
+const password = document.querySelector("#password");
+const confirm_password = document.querySelector("#confirm-password");
+
+
+togglePassword.addEventListener("click", function () {
+  // toggle the type attribute
+  const type =
+    password.getAttribute("type") === "password" ? "text" : "password";
+
+  password.setAttribute("type", type);
+  try {
+    confirm_password.setAttribute("type", type);
+  } catch (e) {
+    //pass
+  }
+
+  // toggle the icon
+  this.classList.toggle("fa-eye-slash");
+});
