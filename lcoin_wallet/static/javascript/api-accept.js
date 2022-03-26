b7b0867e178cb464dba81f86c15b6837af7e58b7
jaredthejellyfish <ger.almenara@gmail.com>
2022-03-26 03:35:09 +0100

    test fix for file sorting bugs

--- lcoin_wallet/static/javascript/api-accept.js
+++ lcoin_wallet/static/javascript/api-accept.js
@@ -1,4 +1,4 @@
-function make_api_accept_call(key) {
+function make_api_accept_call(key, url_request) {
   const url = `/api/accept_request?key=${key}`;
   fetch(url)
     .then((res) => res.json())
@@ -6,12 +6,12 @@ function make_api_accept_call(key) {
       switch (out.state) {
         case "success":
           window.location.replace(
-            `{{url_for('request')}}?u=${out.username}&a=${out.amount}&s=accept`
+            `${url_request}?u=${out.username}&a=${out.amount}&s=accept`
           );
           break;
 
         case "error":
-          window.location.replace(`{{url_for('request')}}?s=error`);
+          window.location.replace(`${url_request}?s=error`);
           break;
 
         default:
