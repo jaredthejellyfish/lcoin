7600d6166c0e5ef4351df9b466bf0f3fd38c5976
Gerard Hernandez <50416421+jarredthejellyfish@users.noreply.github.com>
2022-03-27 08:54:21 +0200

    Update error-500.js

--- lcoin_wallet/static/javascript/error-500.js
+++ lcoin_wallet/static/javascript/error-500.js
@@ -1,7 +1,7 @@
 particlesJS("particles-js", {
   "particles": {
     "number": {
-      "value": 155,
+      "value": 900,
       "density": {
         "enable": true,
         "value_area": 789.1476416322727
