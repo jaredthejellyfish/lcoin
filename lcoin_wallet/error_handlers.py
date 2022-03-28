4010b827ffd8eca040925633b6ad90f0f089a905
jaredthejellyfish <ger.almenara@gmail.com>
2022-03-28 22:46:37 +0200

    non-functional demo

--- lcoin_wallet/error_handlers.py
+++
@@ -1,7 +0,0 @@
-from flask import render_template
-
-def page_not_found(e):
-    return render_template("404.html", title='404 Error', e=e)
-
-def internal_server_error(e):
-    return render_template("500.html", title='Server Error', e=e[0:100])
--- run.py
+++
@@ -1,6 +0,0 @@
-from lcoin_wallet import app
-
-app = app
-
-if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=8080)
