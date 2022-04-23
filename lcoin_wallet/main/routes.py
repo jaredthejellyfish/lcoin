36f666176c943a59f71a9b5f87e3b24957595671
Gerard Hernandez <ger.almenara@gmail.com>
2022-04-23 17:39:33 +0200

    added concept to requests as a popover

--- lcoin_wallet/main/routes.py
+++ lcoin_wallet/main/routes.py
@@ -1,10 +1,12 @@
 from flask import render_template, url_for, redirect
-from lcoin_wallet.main.utils import check_if_pending, get_transactions
+from lcoin_wallet.main.utils import (
+    check_if_pending, get_transactions, transaction_html_constructor, get_btc_price)
 from flask_login import login_required, current_user
 from flask import Blueprint, send_from_directory
 
 
 main = Blueprint('main', __name__)
+
 
 @main.route('/')
 @main.route('/home')
@@ -15,7 +17,9 @@ def home():
 
     exists, _ = check_if_pending(current_user)
     transactions = get_transactions(current_user)
-
+    btc_price = round(get_btc_price(), 6)
+    transactions_html = transaction_html_constructor(transactions, btc_price)
+    
     if exists[1] is True:
         notification = "badge1"
     else:
@@ -24,29 +28,35 @@ def home():
     image_file = url_for('main.profile_photo')
 
     if len(transactions) > 1:
-        return render_template("index.html", title='Wallet', image_file=image_file, notification=notification, transactions=transactions[::-1])
+        return render_template("index.html", title='Wallet', image_file=image_file, notification=notification, transactions_html=transactions_html, btc_price=btc_price)
     else:
         return render_template("index.html", title='Wallet', image_file=image_file, notification=notification, transactions=[])
+
 
 @main.route('/sw.js')
 def sw():
     return send_from_directory('static', 'sw.js')
 
+
 @main.route('/manifest.json')
 def manifest():
-    return send_from_directory('static','manifest.json')
+    return send_from_directory('static', 'manifest.json')
+
 
 @main.route('/app/static/js')
 def app_js():
     return send_from_directory('static', 'javascript/app.js')
 
+
 @main.route('/l-coin-logo.png')
 def logo():
     return send_from_directory('static', 'images/l-coin-logo.png')
 
+
 @main.route('/profile_photo')
 def profile_photo():
     return send_from_directory('static', f'profile_pics/{current_user.image_file}')
+
 
 @main.route('/error_500')
 def error_500():
--- lcoin_wallet/main/utils.py
+++ lcoin_wallet/main/utils.py
@@ -1,12 +1,15 @@
 from lcoin_wallet.models import Request, Transaction
-
 from lcoin_wallet.models import Transaction, Request
+import requests
+import datetime
+
 
 def check_if_pending(current_user):
     by_user = Request.query.filter_by(by=current_user.username, active=True)
     to_user = Request.query.filter_by(to=current_user.username, active=True)
 
     return [by_user.first() is not None, to_user.first() is not None], [by_user, to_user]
+
 
 def get_transactions(current_user):
     sent = []
@@ -17,12 +20,131 @@ def get_transactions(current_user):
 
     for transaction in sent_:
         sent.append(["Sent", transaction.date,
-                    transaction.to, transaction.amount])
+                    transaction.to, transaction.amount, transaction.concept])
 
     for transaction in received_:
         received.append(["Received", transaction.date,
-                        transaction.by, transaction.amount])
+                        transaction.by, transaction.amount, transaction.concept])
 
     lists = sorted(sent + received, key=lambda x: x[1])
 
-    return lists
+    return lists[::-1]
+
+
+def get_btc_price():
+    response = requests.get(
+        'https://api.coindesk.com/v1/bpi/currentprice.json')
+    data = response.json()
+    return (1 / float(data["bpi"]["EUR"]["rate"].replace(',', ''))) * 10
+
+
+def transaction_html_constructor(transactions, btc_price):
+    transactions_html = ''
+    transactions = transactions[:5]
+
+    for i in range(len(transactions)):
+            if i == 0:
+                if transactions[i][4] is '':
+                    fromatted_base = f"""
+                    <span  tabindex="0" class="" role="button" data-toggle="popover" data-trigger="focus" data-placement="top" data-content="{transactions[i][1].strftime('%d-%m-%Y')}">
+                        <div name="transaction" class="flex-container" style="margin-top: -20px; padding-left: 4%;">
+                            <div class="flex-child">
+                                <p class="bold_text" style="margin-left: 0;">{transactions[i][0]}</p>
+                                <p class="dark-text" style="margin-top: -10px; margin-left: 0; font-size: 80%;">{transactions[i][2]}</p>
+                            </div>
+                            <div class="flex-child" style="text-align: right; margin-bottom: 20px;">
+                                <p class="bold_text" style="margin-right: 10%;">{'-' if transactions[i][0] == 'Sent' else '+'} ₺{transactions[i][3]}</p>
+                                <p class="dark-text" style="margin-top: -10px; margin-right: 10%; font-size: 80%;">{'-' if transactions[i][0] == 'Sent' else '+'} {round((btc_price * float(transactions[i][3])), 6)} BTC</p>
+                            </div>
+                        </div>
+                    </span>
+                    <hr class="dashed">
+                    """.replace('\n', '')
+
+                else:
+                    fromatted_base = f"""
+                    <span  tabindex="0" class="" role="button" data-toggle="popover" data-trigger="focus" data-placement="top" title="{transactions[i][1].strftime('%d-%m-%Y')}" data-content="{transactions[i][4].capitalize()}">
+                        <div name="transaction" class="flex-container" style="margin-top: -20px; padding-left: 4%;">
+                            <div class="flex-child">
+                                <p class="bold_text" style="margin-left: 0;">{transactions[i][0]}</p>
+                                <p class="dark-text" style="margin-top: -10px; margin-left: 0; font-size: 80%;">{transactions[i][2]}</p>
+                            </div>
+                            <div class="flex-child" style="text-align: right; margin-bottom: 20px;">
+                                <p class="bold_text" style="margin-right: 10%;">{'-' if transactions[i][0] == 'Sent' else '+'} ₺{transactions[i][3]}</p>
+                                <p class="dark-text" style="margin-top: -10px; margin-right: 10%; font-size: 80%;">{'-' if transactions[i][0] == 'Sent' else '+'} {round((btc_price * float(transactions[i][3])), 6)} BTC</p>
+                            </div>
+
+                        </div>
+                    </span>
+                    <hr class="dashed">
+                    """.replace('\n', '')
+
+            elif i == len(transactions) - 1:
+                if transactions[i][4] is '':
+                    fromatted_base = f"""
+                    <span  tabindex="0" class="" role="button" data-toggle="popover" data-trigger="focus" data-placement="top" data-content="{transactions[i][1].strftime('%d-%m-%Y')}">
+                        <div name="transaction" class="flex-container" style="margin-top: -20px; padding-left: 4%; margin-bottom: 10px;">
+                            <div class="flex-child">
+                                <p class="bold_text" style="margin-left: 0;">{transactions[i][0]}</p>
+                                <p class="dark-text" style="margin-top: -10px; margin-left: 0; font-size: 80%;">{transactions[i][2]}</p>
+                            </div>
+                            <div class="flex-child" style="text-align: right; margin-bottom: 20px;">
+                                <p class="bold_text" style="margin-right: 10%;">{'-' if transactions[i][0] == 'Sent' else '+'} ₺{transactions[i][3]}</p>
+                                <p class="dark-text" style="margin-top: -10px; margin-right: 10%; font-size: 80%;">{'-' if transactions[i][0] == 'Sent' else '+'} {round((btc_price * float(transactions[i][3])), 6)} BTC</p>
+                            </div>
+                        </div>
+                    </span>
+                    """.replace('\n', '')
+                else:
+                    fromatted_base = f"""
+                    <span  tabindex="0" class="" role="button" data-toggle="popover" data-trigger="focus" data-placement="top" title="{transactions[i][1].strftime('%d-%m-%Y')}" data-content="{transactions[i][4].capitalize()}">
+                        <div name="transaction" class="flex-container" style="margin-top: -20px; padding-left: 4%; margin-bottom: 10px;">
+                            <div class="flex-child">
+                                <p class="bold_text" style="margin-left: 0;">{transactions[i][0]}</p>
+                                <p class="dark-text" style="margin-top: -10px; margin-left: 0; font-size: 80%;">{transactions[i][2]}</p>
+                            </div>
+                            <div class="flex-child" style="text-align: right; margin-bottom: 20px;">
+                                <p class="bold_text" style="margin-right: 10%;">{'-' if transactions[i][0] == 'Sent' else '+'} ₺{transactions[i][3]}</p>
+                                <p class="dark-text" style="margin-top: -10px; margin-right: 10%; font-size: 80%;">{'-' if transactions[i][0] == 'Sent' else '+'} {round((btc_price * float(transactions[i][3])), 6)} BTC</p>
+                            </div>
+                        </div>
+                    </span>
+                    """.replace('\n', '')
+
+            else:
+                if transactions[i][4] is '':
+                    fromatted_base = f"""
+                    <span  tabindex="0" class="" role="button" data-toggle="popover" data-trigger="focus" data-placement="top" data-content="{transactions[i][1].strftime('%d-%m-%Y')}">
+                        <div name="transaction" class="flex-container" style="margin-top: -20px; padding-left: 4%;">
+                            <div class="flex-child">
+                                <p class="bold_text" style="margin-left: 0;">{transactions[i][0]}</p>
+                                <p class="dark-text" style="margin-top: -10px; margin-left: 0; font-size: 80%;">{transactions[i][2]}</p>
+                            </div>
+                            <div class="flex-child" style="text-align: right; margin-bottom: 20px;">
+                                <p class="bold_text" style="margin-right: 10%;">{'-' if transactions[i][0] == 'Sent' else '+'} ₺{transactions[i][3]}</p>
+                                <p class="dark-text" style="margin-top: -10px; margin-right: 10%; font-size: 80%;">{'-' if transactions[i][0] == 'Sent' else '+'} {round((btc_price * float(transactions[i][3])), 6)} BTC</p>
+                            </div>
+                        </div>
+                    </span>
+                    <hr class="dashed">
+                    """.replace('\n', '')
+                else:
+                    fromatted_base = f"""
+                    <span  tabindex="0" class="" role="button" data-toggle="popover" data-trigger="focus" data-placement="top" title="{transactions[i][1].strftime('%d-%m-%Y')}" data-content="{transactions[i][4].capitalize()}">
+                        <div name="transaction" class="flex-container" style="margin-top: -20px; padding-left: 4%;">
+                            <div class="flex-child">
+                                <p class="bold_text" style="margin-left: 0;">{transactions[i][0]}</p>
+                                <p class="dark-text" style="margin-top: -10px; margin-left: 0; font-size: 80%;">{transactions[i][2]}</p>
+                            </div>
+                            <div class="flex-child" style="text-align: right; margin-bottom: 20px;">
+                                <p class="bold_text" style="margin-right: 10%;">{'-' if transactions[i][0] == 'Sent' else '+'} ₺{transactions[i][3]}</p>
+                                <p class="dark-text" style="margin-top: -10px; margin-right: 10%; font-size: 80%;">{'-' if transactions[i][0] == 'Sent' else '+'} {round((btc_price * float(transactions[i][3])), 6)} BTC</p>
+                            </div>   
+                        </div>
+                    </span>
+                    <hr class="dashed">
+                    """.replace('\n', '')
+                
+            transactions_html += fromatted_base
+
+    return transactions_html
