122e51b4a997a584ad47bed7075a3c33a634112d
Gerard Hernandez <ger.almenara@gmail.com>
2022-04-23 18:24:49 +0200

    fixed email password reset pr.2

--- lcoin_wallet/users/utils.py
+++ lcoin_wallet/users/utils.py
@@ -85,13 +85,10 @@ def send_reset_email(user):
         msg = Message('Password Reset Request',
                     sender='noreply@lcoin.com',
                     recipients=[user.email])
-        msg.body = f'''
-    
-    To reset your password visit the following link:\n
-    {url_for('users.reset_token', token = token, _external=True)}\n
-        
-    If you did not make this request simply ignore this email and no changes will be made.
-    '''
+        msg.body = f'''To reset your password visit the following link:
+                       {url_for('users.reset_token', token = token, _external=True)}
+                        
+                       If you did not make this request simply ignore this email and no changes will be made.'''
         mail.send(msg)
     else:
         return
