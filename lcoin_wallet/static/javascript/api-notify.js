45b8dec38f7114fd83ac1f6651e753c54aedb761
Gerard Almenara <ger.almenara@gmail.com>
2022-05-18 15:54:44 +0200

    added hotjar tracking for all pages

--- lcoin_wallet/static/javascript/api-notify.js
+++ lcoin_wallet/static/javascript/api-notify.js
@@ -1,37 +1,31 @@
 
 $(function () {
-        setInterval(function () {
-            const url = `/api/pending_requests`;
-            const notify_element = document.getElementById("notify");
-            const notify = notify_element.classList.contains("badge1")
-            fetch(url)
-                .then((res) => res.json())
-                .then((out) => {
-                    
-                    switch (out.notification_badge) {
-                        case "true":
+    setInterval(function () {
+        const url = `/api/pending_requests`;
+        const notify_element = document.getElementById("notify");
+        const notify = notify_element.classList.contains("badge1")
+        fetch(url)
+            .then((res) => res.json())
+            .then((out) => {
+                switch (out.notification_badge) {
+                    case "true":
+                        if (notify == false) {
+                            $('#notify').addClass('badge1');
+                        };
+                        break;
 
-                            if (notify == false){
-                                $('#notify').addClass('badge1');
-                            };
-                            
-                            break;
+                    case "false":
+                        if (notify == true) {
+                            $('#notify').removeClass('badge1');
+                        };
+                        break;
 
-                        case "false":
-                            if (notify == true){
-                                $('#notify').removeClass('badge1');
-                            };
-                            
-                            break;
-
-                        default:
-                            
-                            throw "Invalid response!";
-                    }
-                })
-                .catch((err) => {
-                    throw err;
-                });
-
-        }, 10000);
+                    default:
+                        throw "Invalid response!";
+                }
+            })
+            .catch((err) => {
+                throw err;
+            });
+    }, 20000);
 });
